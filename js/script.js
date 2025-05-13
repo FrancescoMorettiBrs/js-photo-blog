271b20837e800d7a67fe25b91b412504f5196c7f
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-05-13 20:23:31 +0200

    feat: overlay completed

--- js/script.js
+++ js/script.js
@@ -1,29 +1,39 @@
 const userCard = document.getElementById("userCard");
+const apiUrl = "https://lanciweb.github.io/demo/api/pictures/";
+const overlay = document.getElementById("overlay");
+const overlayImg = document.getElementById("overlay-img");
+const closeBtn = document.getElementById("close-overlay");
 
-const apiUrl = "https://lanciweb.github.io/demo/api/pictures/"
-axios
-  .get(apiUrl)
-  .then(function (resp) {
-    const arrayImg = resp.data;
+axios.get(apiUrl).then(function (resp) {
+  const arrayImg = resp.data;
 
-    for (let i = 0; i < arrayImg.length; i++) {
-      const img = arrayImg[i];
-      console.log(img);
+  for (let i = 0; i < arrayImg.length; i++) {
+    const img = arrayImg[i];
 
-      const card = `<div class="col-md-4 col-sm-6 mb-4">
-          <div class="card h-100 p-4">
-            <img src="./img/pin.svg" class="pin" alt="">
-            <img src="${img.url}" class="card-img-top" alt="${img.title}">
-            <div class="card-body">
-              <p class="card-text">${img.title}</p>
-              <p class="card-text">${img.date}</p>
-            </div>
+    const card = `
+      <div class="col-md-4 col-sm-6 mb-4">
+        <div class="card h-100 p-4">
+          <img src="./img/pin.svg" class="pin" alt="">
+          <img src="${img.url}" class="card-img-top clickable-img" alt="${img.title}">
+          <div class="card-body">
+            <p class="card-text">${img.title}</p>
+            <p class="card-text">${img.date}</p>
           </div>
         </div>
-        `;
-      userCard.innerHTML += card;
-    }
-  })
-  .catch(function (error) {
-    console.error("Errore nella chiamata API:", error);
+      </div>`;
+
+    userCard.innerHTML += card;
+  }
+
+  document.querySelectorAll(".clickable-img").forEach(function (imgEl) {
+    imgEl.addEventListener("click", function () {
+      const imgSrc = imgEl.getAttribute("src");
+      overlayImg.setAttribute("src", imgSrc);
+      overlay.classList.remove("overlay-hidden");
+    });
   });
+});
+
+closeBtn.addEventListener("click", function () {
+  overlay.classList.add("overlay-hidden"); 
+});
